1d9afc1bda3404c176fc30dc1136a932023946c8
William Castillo <williamcastillo0997@gmail.com>
2022-07-06 09:40:14 -0600

    cors commit

--- models/server.js
+++ models/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const http = require('http')
 const socketio = require('socket.io')
 const path = require('path');
+
+const cors = require('cors')
+
 const Sockets = require('./sockets');
 class Server {
 
@@ -13,12 +16,30 @@ class Server {
         this.server = http.createServer(this.app)
 
         //configuracion de sockets
-        this.io = socketio(this.server, {/*configuraciones*/ })
+        this.io = socketio(this.server, {cors: {
+            origin: "*",
+            methods: ["GET", "POST"]
+          }});
+        
     }
 
     middlewares() {
         //desplegar el directorio publico
         this.app.use(express.static(path.resolve(__dirname,'../public')))
+
+        
+        //cors
+        this.app.use(cors());
+
+        
+        this.app.use(function(req, res, next) {
+            res.header("Access-Control-Allow-Origin", "*");
+            res.header("Access-Control-Allow-Headers", "X-Requested-With");
+            res.header("Access-Control-Allow-Headers", "Content-Type");
+            res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+            next();
+          });
+    
     }
 
     configurarSockets(){
